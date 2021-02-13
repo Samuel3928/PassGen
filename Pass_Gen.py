969979caa317a6dfaa4600365e791f4f7149a617
Samuel3928 <66529340+Samuel3928@users.noreply.github.com>
2021-02-13 14:19:55 -0600

    Add files via upload

---
+++ Pass_Gen.py
@@ -0,0 +1,73 @@
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+#
+import random, os, webbrowser
+import time
+
+def borrarpant():
+    if os.name == "nt":
+        os.system ("clear")
+    else:
+        os.system ("clear")
+
+#Banner
+
+borrarpant()
+print(RED+":) INICIANDO PROGRAMA..."+RESET)
+time.sleep(2)
+print("")
+print(GREEN+";) INICIADO CORRECTAMENTE"+RESET)
+time.sleep(1)
+borrarpant()
+time.sleep(2)
+print(BLUE+"")
+print("                                                                          ")
+print("       ██▓███   ▄▄▄        ██████   ██████   ▄████ ▓█████  ███▄    █      ")
+print("      ▓██░  ██▒▒████▄    ▒██    ▒ ▒██    ▒  ██▒ ▀█▒▓█   ▀  ██ ▀█   █      ")
+print("      ▓██░ ██▓▒▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒██░▄▄▄░▒███   ▓██  ▀█ ██▒     ")
+print("      ▒██▄█▓▒ ▒░██▄▄▄▄██   ▒   ██▒  ▒   ██▒░▓█  ██▓▒▓█  ▄ ▓██▒  ▐▌██▒     ")
+print("      ▒██▒ ░  ░ ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒▓███▀▒░▒████▒▒██░   ▓██░     ")
+print("      ▒▓▒░ ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░ ░▒   ▒ ░░ ▒░ ░░ ▒░   ▒ ▒      ")
+print("      ░▒ ░       ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░  ░   ░  ░ ░  ░░ ░░   ░ ▒░     ")
+print("      ░░         ░   ▒   ░  ░  ░  ░  ░  ░  ░ ░   ░    ░      ░   ░ ░      ")
+print("                     ░  ░      ░        ░        ░    ░  ░         ░      "+RESET)
+print(RED+"                            PassGen(v.1.0)"+RESET) 
+print (MAGENTA+"         ------------- by @Samuel3928 ------------- "+RESET)
+print (GREEN+"                   Para Salir Presiona Ctrl+c"+RESET)
+print (" ")
+
+
+
+length=int(input (YELLOW+"> Longitud de la Password: "+RESET))
+
+print ("")
+time.sleep(2)
+
+print (CYAN+"      LISTO, VUELTA HECHA! "+RESET)
+print (" ")
+time.sleep(1)
+
+char="abcdefghijklmnopqrstuvwxyz1234567890@#$%~&*^"
+
+password= (RED+"║Password:║ "+RESET)
+print(RED+"╔═════════════╗"+RESET)
+
+for i in range(length):
+
+     password+=random.choice(char)
+
+
+print(password)
+print(RED+"╚═════════════╝"+RESET)
+
+print ("")
+quit()
+
+
